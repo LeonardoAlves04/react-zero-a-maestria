5432ff8cc66d84857fb346b3ae3cb115660275dd
Leonardo Alves <leonardoalves201510@gmail.com>
2023-05-09 09:15:27 -0300

    feat: renderização de lista

--- exercicio/src/App.js
+++ exercicio/src/App.js
@@ -5,17 +5,30 @@ import ShowUserName from './components/ShowUserName';
 import CarDetails from './components/CarDetails';
 
 function App() {
-
   const name = "Leonardo"
+
+  const cars = [
+    { id: 1, brand: "chevrolett", color: "branco", newCar: false, km: 3210 },
+    { id: 2, brand: "hyundai", color: "preto", newCar: false, km: 3213210 },
+    { id: 3, brand: "honda", color: "cinza", newCar: true, km: 0 },
+    { id: 3, brand: "honda", color: "cinza", newCar: true, km: 0 },
+    { id: 3, brand: "honda", color: "cinza", newCar: true, km: 0 },
+    { id: 3, brand: "honda", color: "cinza", newCar: true, km: 0 },
+    { id: 3, brand: "honda", color: "cinza", newCar: true, km: 0 },
+
+  ]
   return (
     <div className="App">
       <ListRender />
       <ConditionalRender />
       <ShowUserName name={name} />
-      <CarDetails km={1000000} brand=" Toyota" color=" azul" newCar={false} />
+      {/* <CarDetails km={1000000} brand=" Toyota" color=" azul" newCar={false} />
       <CarDetails km={1000000} brand=" fiat" color=" vermelho" newCar={false} />
       <CarDetails km={0} brand=" vw" color=" preto" newCar={true} />
-      <CarDetails km={1000000} brand=" chevrollet " color=" cinza" newCar={false} />
+      <CarDetails km={1000000} brand=" chevrollet " color=" cinza" newCar={false} /> */}
+      {cars.map((car) => (
+        <CarDetails brand={car.brand} color={car.color} newCar={car.newCar} km={car.km} />
+      ))}
     </div >
   );
 }
