c2d2883100084696981f593d08995235b6b1d4bb
Leonardo Alves <leonardoalves201510@gmail.com>
2023-05-09 09:43:36 -0300

    feat: trabalhando com a prop children

--- exercicio/src/App.js
+++ exercicio/src/App.js
@@ -3,6 +3,8 @@ import ListRender from './components/ListRender';
 import ConditionalRender from './components/ConditionalRender';
 import ShowUserName from './components/ShowUserName';
 import CarDetails from './components/CarDetails';
+import Fragment from './components/Fragment';
+import Container from './components/Container';
 
 function App() {
   const name = "Leonardo"
@@ -11,24 +13,24 @@ function App() {
     { id: 1, brand: "chevrolett", color: "branco", newCar: false, km: 3210 },
     { id: 2, brand: "hyundai", color: "preto", newCar: false, km: 3213210 },
     { id: 3, brand: "honda", color: "cinza", newCar: true, km: 0 },
-    { id: 3, brand: "honda", color: "cinza", newCar: true, km: 0 },
-    { id: 3, brand: "honda", color: "cinza", newCar: true, km: 0 },
-    { id: 3, brand: "honda", color: "cinza", newCar: true, km: 0 },
-    { id: 3, brand: "honda", color: "cinza", newCar: true, km: 0 },
-
   ]
+
   return (
     <div className="App">
       <ListRender />
       <ConditionalRender />
       <ShowUserName name={name} />
-      {/* <CarDetails km={1000000} brand=" Toyota" color=" azul" newCar={false} />
+      <CarDetails km={1000000} brand=" Toyota" color=" azul" newCar={false} />
       <CarDetails km={1000000} brand=" fiat" color=" vermelho" newCar={false} />
       <CarDetails km={0} brand=" vw" color=" preto" newCar={true} />
-      <CarDetails km={1000000} brand=" chevrollet " color=" cinza" newCar={false} /> */}
+      <CarDetails km={1000000} brand=" chevrollet " color=" cinza" newCar={false} />
       {cars.map((car) => (
         <CarDetails brand={car.brand} color={car.color} newCar={car.newCar} km={car.km} />
       ))}
+      <Fragment propFragment={"teste testando 123123213"} />
+      <Container>
+        <p>componente filho</p>
+      </Container>
     </div >
   );
 }
---
+++ exercicio/src/components/Fragment.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Fragment = ({ propFragment }) => {
+  return (
+    <>
+      <h1>dasda</h1>
+      <h2>dsadas</h2>
+      {propFragment}
+    </>
+  );
+};
+
+export default Fragment;
