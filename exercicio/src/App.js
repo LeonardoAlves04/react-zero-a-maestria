4f3f1353ff09697a735c2109fc2a9697b2ea110b
Leonardo Alves <leonardoalves201510@gmail.com>
2023-05-09 10:28:08 -0300

    feat: executando funções como props

--- exercicio/src/App.js
+++ exercicio/src/App.js
@@ -5,6 +5,7 @@ import ShowUserName from './components/ShowUserName';
 import CarDetails from './components/CarDetails';
 import Fragment from './components/Fragment';
 import Container from './components/Container';
+import ExecuteFunction from './components/ExecuteFunction';
 
 function App() {
   const name = "Leonardo"
@@ -15,6 +16,9 @@ function App() {
     { id: 3, brand: "honda", color: "cinza", newCar: true, km: 0 },
   ]
 
+  function showMessage() {
+    console.log("executando a função!")
+  }
   return (
     <div className="App">
       <ListRender />
@@ -25,12 +29,13 @@ function App() {
       <CarDetails km={0} brand=" vw" color=" preto" newCar={true} />
       <CarDetails km={1000000} brand=" chevrollet " color=" cinza" newCar={false} />
       {cars.map((car) => (
-        <CarDetails brand={car.brand} color={car.color} newCar={car.newCar} km={car.km} />
+        <CarDetails key={car.id} brand={car.brand} color={car.color} newCar={car.newCar} km={car.km} />
       ))}
       <Fragment propFragment={"teste testando 123123213"} />
       <Container>
         <p>componente filho</p>
       </Container>
+      <ExecuteFunction myFunction={showMessage} />
     </div >
   );
 }
