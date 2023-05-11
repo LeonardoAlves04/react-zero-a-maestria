66ff9ed16b3f453d939c5b8641efb2252e50da54
Leonardo Alves <leonardoalves201510@gmail.com>
2023-05-11 15:43:39 -0300

    feat: aulas de css inline, inline dinamico e classes dinamicas

--- CSSReact/css/src/App.js
+++ CSSReact/css/src/App.js
@@ -2,13 +2,29 @@ import './App.css';
 import MyComponent from './components/MyComponent';
 
 function App() {
+  const n = 8
+  const name = "LeozinhoGameplays"
+  const redTitle = false
   return (
     <div className="App">
+
       <h1>React com CSS</h1>
+
       <MyComponent />
+
       <p>Este parágrafo é do app js</p>
+
       <p className='secondP'>Este também vem do app js</p>
-    </div>
+
+      <p style={{ color: "red", backgroundColor: "cyan", padding: "50px" }}>Este elemento foi estilizado de forma inline</p>
+
+      <h2 style={n > 10 ? { color: "RED" } : { color: "GREEN" }}>CSS INLINE DE CRIA</h2>
+
+      <h2 style={name === "LeozinhoGameplays" ? { color: "blue", backgroundColor: "yellow" } : { color: "black", backgroundColor: "cyan" }}>TESTANDO NOME LEOZINHO GAMEPLAYS</h2>
+
+      <h2 className={redTitle ? "red-title" : "title"}>Este título vai ter a classe dinamica</h2>
+
+    </div >
   );
 }
 
