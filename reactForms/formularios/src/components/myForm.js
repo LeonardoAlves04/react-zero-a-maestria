66fa603d6a87c69d5d0c565b479bd9c32a774bf7
Leonardo Alves <leonardoalves201510@gmail.com>
2023-05-16 20:55:41 -0300

    feat:aula de gerenciamento de dados

--- reactForms/formularios/src/components/myForm.js
+++ reactForms/formularios/src/components/myForm.js
@@ -1,19 +1,27 @@
 import styles from "./MyForm.css"
+import { useState } from "react"
 
 const MyForm = () => {
+    // aula 3: gerenciamento de dados
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+
+    const handleName = (e) => (setName(e.target.value))
     return (
-        <div>
+        // aula 1 : criação de formulários
+        < div >
             <form>
                 <div>
                     <label htmlFor="name">Nome:</label>
-                    <input type="text" name="name" placeholder="Seu nome completo" />
+                    <input type="text" name="name" placeholder="Seu nome completo" onChange={handleName} />
                 </div>
                 <input type="submit" placeholder="Enviar" />
+                {/* aula 2: label envolvendo input */}
                 <label>
                     <input type="email" name="email" placeholder="digite aqui o seu e-mail" />
                 </label>
             </form>
-        </div>
+        </div >
     )
 }
 
