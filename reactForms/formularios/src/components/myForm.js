d4b5226db7ca9a2d4f6ec685b86fdb7ad0421390
Leonardo Alves <leonardoalves201510@gmail.com>
2023-05-17 14:26:18 -0300

    feat: aula de alteração de state inline

--- reactForms/formularios/src/components/myForm.js
+++ reactForms/formularios/src/components/myForm.js
@@ -8,19 +8,27 @@ const MyForm = () => {
 
     const handleName = (e) => (setName(e.target.value))
 
+    console.log(name)
     console.log(email)
+
     return (
         // aula 1 : criação de formulários
         < div >
             <form>
                 <div>
                     <label htmlFor="name">Nome:</label>
-                    <input type="text" name="name" placeholder="Seu nome completo" onChange={handleName} />
+                    <input type="text"
+                        name="name"
+                        placeholder="Seu nome completo"
+                        onChange={handleName} />
                 </div>
                 <input type="submit" placeholder="Enviar" />
                 {/* aula 2: label envolvendo input */}
                 <label>
-                    <input type="email" name="email" placeholder="Digite aqui o seu e-mail" onChange={(e) => setEmail(e.target.value)} />
+                    <input type="email"
+                        name="email"
+                        placeholder="Digite aqui o seu e-mail"
+                        onChange={(e) => setEmail(e.target.value)} />
                 </label>
             </form>
         </div >
