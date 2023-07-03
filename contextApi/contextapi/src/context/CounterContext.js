15dd040e4faeb676bb28b71346a88d1072f1d5e8
Leonardo Alves <leonardoalves201510@gmail.com>
2023-07-03 13:11:32 -0300

    chore: criando e consumindo provider

--- contextApi/contextapi/src/context/CounterContext.js
+++ contextApi/contextapi/src/context/CounterContext.js
@@ -2,4 +2,16 @@
 
 import { createContext, useState } from "react"
 
-expor default CounterContext = createContext();
+export const CounterContext = createContext();
+
+// 2 - Criar provider
+
+export const CounterContextProvider = ({ children }) => {
+    const [counter, setCounter] = useState(5)
+
+    return (
+        <CounterContext.Provider value={{ counter, setCounter }}>
+            {children}
+        </CounterContext.Provider>
+    )
+}
